04d6ce491b4f63847cdc88d28361d5bfe5c8db65
pmattiollo <pmattiollo@gmail.com>
2017-10-27 00:18:16 -0200

    Creation of a Spring Boot project

---
+++ trade-validation-api/src/main/java/br/com/pmattiollo/tradevalidationapi/TradeValidationApiApplication.java
@@ -0,0 +1,12 @@
+package br.com.pmattiollo.tradevalidationapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TradeValidationApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TradeValidationApiApplication.class, args);
+	}
+}
